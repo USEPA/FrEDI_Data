ec0c903458d570ab19425ecfc1c04428cadf562e
Karen Noiva <knoiva@indecon.com>
2023-09-19 17:36:18 -0400

    Cleaned up script for compiling data for main FrEDI

--- createSystemData_main.R
+++ createSystemData_main.R
@@ -1,205 +1,102 @@
-# Script
-
-
-## Load Packages
+###### Script for configuring data for main FrEDI
+###### Load Packages ######
 require(tidyverse)
 require(openxlsx)
 require(devtools)
 
-
-mostRecentDate <- "20230503"# Sys.Date()
-
-
-projectPath      <- "."
-today            <- format(Sys.Date(), "%Y%m%d")
-
-
-### Project configuration
-configPath       <- projectPath %>% file.path("config")
-# configFiles      <- configPath %>% list.files(".R")
-configFiles      <- "frediConfig.R"
-### Code path
-codePath         <- projectPath %>% file.path("R")
-codeNames        <- codePath %>% list.files(".R")
-configLogPath    <- codePath %>% file.path("configLog")
-
-### Names for archive and use versions
-configFileName   <- "fredi_config"
-configArchName   <- configLogPath %>% file.path(configFileName) %>% paste(today, sep="_") %>% paste0(".rda")
-configUseName    <- codePath %>% file.path(configFileName) %>% paste0(".rda")
-
-### Output path
-dataOutPath      <- projectPath %>% file.path("data")
-dataOutLog       <- dataOutPath %>% file.path("dataLog")
-sysFileName      <- "sysdata"
-sysArchName      <- dataOutLog %>% file.path(sysFileName) %>% paste(today, sep="_") %>% paste0(".rda")
-excelFileName    <- mostRecentDate %>% paste("FrEDI_config.xlsx", sep="_")
-sysUseName       <- dataOutPath %>% file.path(sysFileName) %>% paste0(".rda")
-
-### Name of most recent sysdata.rda file in archive
-dataOutLogFiles  <- dataOutLog %>% list.files %>% sort(decreasing = T); dataOutLogFiles %>% head
-sysFileRef       <- dataOutLog %>% file.path(dataOutLogFiles[1]); sysFileRef
-# sysFileRef       <- dataOutLog %>% file.path("sysdata_20221021.rda"); sysFileRef
-
-
-# Configuration Files -->
-##Load all configuration functions
-
-
-### Load all functions
-
-for(i in configFiles){ source(paste(configPath, i, sep="/")) }
-
-
-
-#List configuration files
-
-configLogPath %>% list.files("rda") %>% sort(decreasing=FALSE) %>% head
-
-
-#Update archive:
-
-frediConfig(outPath = configArchName)
-
-
-#Confirm copy was saved:
-  
-basename(configArchName) %in% (configLogPath %>% list.files("rda"))
-
-## Create Tool Configuration Data
-
-### 1. Load Data
-
-#Load data from Excel configuration file:
-  
-### Load all functions
-# codeNames; 
-for(i in codeNames){ source(paste(codePath, i, sep="/")) }
-### list_loadData
-list_loadData <- loadData( 
-  fileDir   = "." %>% file.path( "inst", "extdata"), ### Path to project
-  fileName  = excelFileName, ### name of excel file with config information
-  sheetName = "tableNames",
-  silent = NULL
-)
-
-
-#<!-- Check scaled impacts -->
-  
-  test_impacts0  <- list_loadData$data_scaledImpacts 
-  names_impacts0 <- test_impacts0 %>% names 
-  check_impacts0 <- test_impacts0 %>% group_by_at(.vars=c(all_of(names_impacts0))) %>% summarize(n=n(), .groups="keep") %>% ungroup 
-  #test_impacts0 %>% glimpse -->
-  #check_impacts0 %>% glimpse -->
-  #check_impacts0 %>% filter(n>1) %>% glimpse -->
-
-  
-  ### 2. Reshape Loaded Data
-  
-#Reshape loaded Excel data
-
-### Load all functions
-# codeNames; 
-### list_reshapeData
-list_reshapeData <- list_loadData %>% reshapeData(silent=T)
-
-
-
-  test_impacts0  <- list_reshapeData$data_scaledImpacts 
-  names_impacts0 <- test_impacts0 %>% names 
-  check_impacts0 <- test_impacts0 %>% group_by_at(.vars=c(all_of(names_impacts0))) %>% summarize(n=n(), .groups="keep") %>% ungroup 
-
-
-  
-  
-  ### 3. Configure Data
-  
-#Test `createSystemData()` function (configure reshaped data)
-    
-# frediPath %>% load_all
-### Uncomment following two lines to create and save data and check the outputs
-list_systemData0 <- list_reshapeData %>% createSystemData(save=F, silent=T)
-
-
-### 4. Tests 
-
-#### Parameters
-
-#Parameters for saving tests
-
+###### Set Arguments ######
+###### Parameters for saving tests
 save_test   <- TRUE
 return_test <- TRUE
 
+###### Set Paths ######
+###### Project path
+projectDir  <- "."
+###### Code path
+codeDir     <- projectDir %>% file.path("R")
+codeNames   <- codeDir    %>% list.files(".R")
+codePaths   <- codeDir    %>% file.path(codeNames)
+###### Data input path and file name
+dataInDir   <- projectDir %>% file.path("inst", "extdata")
+dataInName  <- "FrEDI_config" %>% paste0(".xlsx")
+dataInPath  <- dataInDir %>% file.path(dataInName)
+###### Data output path and file name
+dataOutDir  <- projectDir %>% file.path("data")
+dataOutName <- "sysdata"  %>% paste0(".rda")
+dataOutPath <- dataOutDir %>% file.path(dataOutName)
 
+###### 0. Local Function ###### 
+for(i in codePaths){ i %>% source() }
 
-#### Reshaped Data Test
-#Try different methods -->
-  
- test_types <- c("data.frame", "tibble") 
-test_list  <- list(a=test_types, b=c("list"))
- test_list1 <- test_list %>% names %>% map(~case_when(("data.frame" %in% test_list[[.]])~1, ("list" %in% test_list[[.]])~2, TRUE~0)) 
- test_list2 <- test_list %>% names %>% map(~case_when(("data.frame" %in% test_list[[.]])~"data.frame", ("list" %in% test_list[[.]])~"list", TRUE~"N/A"))
-
-  
- # Try the test for the reshaped data:
-  
-test_reshapeData   <- list_reshapeData %>% dataInfo_test(
-  csvName = "reshapedData_testResults",
-  save    = save_test,
-  return  = return_test
-)
-#Examine reshaped data test results: check for any tables that didnt pass
-
-
-
-#### General Config Tests
-
-#Try the general tests for configured data:
-
-test_systemData0 <- list_systemData0 %>% dataInfo_test(
-  csvName = "configuredData_testResults",
-  save    = save_test,
-  return  = return_test
+###### 1. Load Excel Data ######
+### list_loadData
+list_loadData <- loadData( 
+  fileDir   = dataInDir,    ### Path to project
+  fileName  = dataInName,   ### name of excel file with config information
+  sheetName = "tableNames", ### Sheet with info about tables in config file
+  silent    = NULL
 )
 
+###### 2. Reshape Loaded Data ######
+### list_reshapeData
+list_reshapeData <- list_loadData %>% reshapeData(silent=T)
 
-#Examine configured data test results: check for any tables that didnt pass
+###### 3. Configure Data ######
+list_systemData0 <- list_reshapeData %>% createSystemData(save=F, silent=T)
+
+###### 4. Run Individual Tests on Data ######
+# ###### Test the reshaped data:
+# test_reshapeData <- list_reshapeData %>% dataInfo_test(
+#   csvName = "reshapedData_testResults",
+#   save    = save_test,
+#   return  = return_test
+# )
+# 
+# ###### Run the general config tests for configured data:
+# test_systemData0 <- list_systemData0 %>% dataInfo_test(
+#   csvName = "configuredData_testResults",
+#   save    = save_test,
+#   return  = return_test
+# )
 
 
-
-#### General Tests
-
-#Wrap the general tests into a single wrapper function:
-  
+###### 5. Run General Tests on Data ######
 test_general_config <- general_config_test(
   reshapedData   = list_reshapeData,
   configuredData = list_systemData0,
   overwrite      = TRUE,
-  fredi_config = fredi_config
+  xlsxName       = "generalConfig_testResults.xlsx",
+  fredi_config   = fredi_config
 )
 
+###### 6. Load Reference Data ######
+### Load ref data
+newEnv_ref  <- new.env()
+dataOutPath %>% load(verbose = F, envir=newEnv_ref)
+# ls(envir=newEnv_ref) %>% print
+refDataList <- "rDataList" %>% get(envir=newEnv_ref, inherits = F)
+# ls() %>% print; refDataList %>% names %>% print
+rm("newEnv_ref")
 
-#### New Sector Tests
+###### 6. Run New Sector Tests on Data ######
+###### Determine if tests need to be run
+c_sectors_new <- list_systemData0[["co_sectors"]][["sector_id"]] %>% as.matrix %>% as.vector %>% unique
+c_sectors_ref <- refDataList[["co_sectors"]][["sector_id"]]  %>% as.matrix %>% as.vector %>% unique
+hasNewSectors <- (c_sectors_new %in% c_sectors_ref) %>% all 
+doNewTest     <- !hasNewSectors
+###### Run test if there are new sectors
+if(doNewTest){
+  test_newSectors_config <- newSectors_config_test(
+    newData     = list_systemData0,
+    refDataFile = dataOutPath,
+    xslxName    = "newSectorsConfig_testResults.xlsx",
+    save        = save_test,
+    return      = return_test
+  )
+}
 
-# Run `createSystemData()` tests:
-  
-test_newSectors_config <- newSectors_config_test(
-  newData     = list_systemData0,
-  refDataFile = sysFileRef,
-  save      = save_test,
-  return    = return_test
-)
-
-
-## Update Data
-
-
-# If above passed tests, overwrite previous config
-
-# codeNames; for(i in codeNames){ source(paste(codePath, i, sep="/")) }
-### Uncomment following two lines to create and save data and check the outputs
-list_systemData <- list_reshapeData %>% createSystemData(save=T, silent=T)
-#list_systemData <- list_reshapeData %>% createSystemData(save=T, silent=T)
+###### 7. Update Saved Data ######
+# list_systemData <- list_reshapeData %>% createSystemData(save=T, silent=T)
 
 
 
