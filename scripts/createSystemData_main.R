cd95fc0e427166fd143c0d908878a7fcd4a5fb0f
Karen Noiva <knoiva@indecon.com>
2023-09-20 15:51:43 -0400

    Update createSystemData_main.R
    Changed operator to native pipe operator

--- scripts/createSystemData_main.R
+++ scripts/createSystemData_main.R
@@ -13,20 +13,20 @@ return_test <- TRUE
 ###### Project path
 projectDir  <- "."
 ###### Code path
-codeDir     <- projectDir %>% file.path("R")
-codeNames   <- codeDir    %>% list.files(".R")
-codePaths   <- codeDir    %>% file.path(codeNames)
+codeDir     <- projectDir |> file.path("R")
+codeNames   <- codeDir    |> list.files(".R")
+codePaths   <- codeDir    |> file.path(codeNames)
 ###### Data input path and file name
-dataInDir   <- projectDir %>% file.path("inst", "extdata")
-dataInName  <- "FrEDI_config" %>% paste0(".xlsx")
-dataInPath  <- dataInDir %>% file.path(dataInName)
+dataInDir   <- projectDir |> file.path("inst", "extdata")
+dataInName  <- "FrEDI_config" |> paste0(".xlsx")
+dataInPath  <- dataInDir |> file.path(dataInName)
 ###### Data output path and file name
-dataOutDir  <- projectDir %>% file.path("data")
-dataOutName <- "sysdata"  %>% paste0(".rda")
-dataOutPath <- dataOutDir %>% file.path(dataOutName)
+dataOutDir  <- projectDir |> file.path("data")
+dataOutName <- "sysdata"  |> paste0(".rda")
+dataOutPath <- dataOutDir |> file.path(dataOutName)
 
 ###### 0. Local Function ###### 
-for(i in codePaths){ i %>% source() }
+for(i in codePaths){ i |> source() }
 
 ###### 1. Load Excel Data ######
 ### list_loadData
@@ -39,21 +39,21 @@ list_loadData <- loadData(
 
 ###### 2. Reshape Loaded Data ######
 ### list_reshapeData
-list_reshapeData <- list_loadData %>% reshapeData(silent=T)
+list_reshapeData <- list_loadData |> reshapeData(silent=T)
 
 ###### 3. Configure Data ######
-list_systemData0 <- list_reshapeData %>% createSystemData(save=F, silent=T)
+list_systemData0 <- list_reshapeData |> createSystemData(save=F, silent=T)
 
 ###### 4. Run Individual Tests on Data ######
 # ###### Test the reshaped data:
-# test_reshapeData <- list_reshapeData %>% dataInfo_test(
+# test_reshapeData <- list_reshapeData |> dataInfo_test(
 #   csvName = "reshapedData_testResults",
 #   save    = save_test,
 #   return  = return_test
 # )
 # 
 # ###### Run the general config tests for configured data:
-# test_systemData0 <- list_systemData0 %>% dataInfo_test(
+# test_systemData0 <- list_systemData0 |> dataInfo_test(
 #   csvName = "configuredData_testResults",
 #   save    = save_test,
 #   return  = return_test
@@ -72,17 +72,17 @@ test_general_config <- general_config_test(
 ###### 6. Load Reference Data ######
 ### Load ref data
 newEnv_ref  <- new.env()
-dataOutPath %>% load(verbose = F, envir=newEnv_ref)
-# ls(envir=newEnv_ref) %>% print
-refDataList <- "rDataList" %>% get(envir=newEnv_ref, inherits = F)
-# ls() %>% print; refDataList %>% names %>% print
+dataOutPath |> load(verbose = F, envir=newEnv_ref)
+# ls(envir=newEnv_ref) |> print
+refDataList <- "rDataList" |> get(envir=newEnv_ref, inherits = F)
+# ls() |> print; refDataList |> names |> print
 rm("newEnv_ref")
 
 ###### 6. Run New Sector Tests on Data ######
 ###### Determine if tests need to be run
-c_sectors_new <- list_systemData0[["co_sectors"]][["sector_id"]] %>% as.matrix %>% as.vector %>% unique
-c_sectors_ref <- refDataList[["co_sectors"]][["sector_id"]]  %>% as.matrix %>% as.vector %>% unique
-hasNewSectors <- (c_sectors_new %in% c_sectors_ref) %>% all 
+c_sectors_new <- list_systemData0[["co_sectors"]][["sector_id"]] |> as.matrix |> as.vector |> unique
+c_sectors_ref <- refDataList[["co_sectors"]][["sector_id"]]  |> as.matrix |> as.vector |> unique
+hasNewSectors <- (c_sectors_new %in% c_sectors_ref) |> all 
 doNewTest     <- !hasNewSectors
 ###### Run test if there are new sectors
 if(doNewTest){
@@ -96,7 +96,7 @@ if(doNewTest){
 }
 
 ###### 7. Update Saved Data ######
-list_systemData <- list_reshapeData %>% createSystemData(outPath= dataOutDir %>% file.path("tmp_sysdata.rda"),
+list_systemData <- list_reshapeData |> createSystemData(outPath= dataOutDir |> file.path("tmp_sysdata.rda"),
 save=T, silent=T)
 
 
