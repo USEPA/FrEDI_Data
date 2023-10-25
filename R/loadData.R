1571b06eaf6944b84d32d1945065e53a566e5f5c
Karen Noiva <knoiva@indecon.com>
2023-10-25 17:30:05 -0400

    Edits to improve createSystemData outputs
    Made small edits to loadData, reshapeData, and createSystemData to improve the outputs of createSystemData, particularly at the state-level

--- R/loadData.R
+++ R/loadData.R
@@ -2,11 +2,11 @@
 ### 2022.01.10: Added SLR sectors
 ### This function loads the data from a directory specified by the user. There is a default directory structure it will try to load from.
 loadData <- function(
-    fileDir   = "." %>% file.path("inst", "extdata"), ### Path to project
-    fileName  = NULL, ### name of excel file with config information
+    fileDir   = "." |> file.path("inst", "extdata"), ### Path to project
+    fileName  = "FrEDI_config.xlsx", ### name of excel file with config information
     sheetName = "tableNames",
-    silent    = FALSE,
-    byState   = FALSE
+    byState   = FALSE,
+    silent    = FALSE
 ) {
   # if(is.null(sheetName)){sheetName <- "tableNames"}
   # print(getwd())
@@ -14,8 +14,8 @@ loadData <- function(
   
   ###### File Path ######
   #fileExt       <- "xlsx"
-  #fileName      <- fileName %>% paste0(".", fileExt)
-  filePath      <- fileDir %>% file.path(fileName)
+  #fileName      <- fileName |> paste0(".", fileExt)
+  filePath      <- fileDir |> file.path(fileName)
   ###### Load Table of Tables ######
   ### Load table with names of data tables
   # print(filePath)
@@ -28,12 +28,12 @@ loadData <- function(
   )
   ### Filter to those for importing and replace NA values in some columns
   mutate0 <- c("excludeCol_ids", "Notes")
-  df_tableNames <- df_tableNames %>% filter(Import == 1)
-  df_tableNames <- df_tableNames %>% mutate_at(.vars = c(mutate0), replace_na, "")
+  df_tableNames <- df_tableNames |> filter(Import == 1)
+  df_tableNames <- df_tableNames |> mutate_at(.vars = c(mutate0), replace_na, "")
   rm("mutate0")
   
   ### Number of data tables
-  num_dataTables <- df_tableNames %>% nrow()
+  num_dataTables <- df_tableNames |> nrow()
   
   ###### Load Each Table ######
   ### Iterate over the list of data tables: import them and add them to the list of tables
@@ -63,13 +63,13 @@ loadData <- function(
     ### Exclude some columns
     ### Columns to exclude by splitting values in `excludeCol_ids` then adjust for row ID in Excel
     ### Exclude the relevant columns
-    # tableInfo_i$excludeCol_ids %>% print
+    # tableInfo_i$excludeCol_ids |> print
     excludeIds_i  <- tableInfo_i[["excludeCol_ids"]]
     doExclude_i   <- !(excludeIds_i == "")
     if (doExclude_i) {
-      excludeCols_i <- excludeIds_i %>% str_split(", ") %>% unlist() %>% as.numeric()
+      excludeCols_i <- excludeIds_i |> str_split(", ") |> unlist() |> as.numeric()
       excludeCols_i <- excludeCols_i - 1
-      table_i       <- table_i %>% select(-c(all_of(excludeCols_i)))
+      table_i       <- table_i |> select(-c(all_of(excludeCols_i)))
       rm("excludeCols_i")
     }
     rm("excludeIds_i", "doExclude_i")
@@ -82,20 +82,26 @@ loadData <- function(
     rm("table_i", "tableInfo_i", "tableName_i", "i")
   } ### End lapply
   
+  ###### Set Aside GDP Default ######
+  ### Default scenario
+  gdp_default   <- dataList[["co_defaultScenario"]]
+  gdp_default   <- gdp_default |> select(c("year", "gdp_usd"))
+  dataList[["gdp_default"]] <- gdp_default
+  
+  ###### Load State Data ######
   if (byState) {
-    state_sectors <- dataList$co_sectors %>% filter(byState == 1)
-    state_sectors <- state_sectors$sector_id %>% unique
-    state_data <- fileDir %>%
-      file.path("state") %>%
-      loadStateData(sectors = state_sectors)
-    yr_gdp <- dataList$co_defaultScenario %>% select(year, gdp_usd)
-    
-    dataList$data_scaledImpacts <- state_data$df_gcmStateImpacts
-    dataList$slrImpacts         <- state_data$df_slrStateImpacts
-    dataList$scalarDataframe    <- state_data$df_stateScalars
-    dataList$co_defaultScenario <- state_data$df_statePop
-    dataList$gdp_default        <- yr_gdp
-  }
+    ### State sectors
+    state_sectors <- dataList     [["co_sectors"]] |> filter(byState == 1)
+    state_sectors <- state_sectors[["sector_id" ]] |> unique()
+    ### Load state data
+    state_fpath   <- fileDir     |> file.path("state")
+    state_data    <- state_fpath |> loadStateData(sectors = state_sectors)
+    ### Replace region values with state data
+    dataList[["data_scaledImpacts"]] <- state_data[["df_gcmStateImpacts"]]
+    dataList[["slrImpacts"        ]] <- state_data[["df_slrStateImpacts"]]
+    dataList[["scalarDataframe"   ]] <- state_data[["df_stateScalars"   ]]
+    dataList[["co_defaultScenario"]] <- state_data[["df_statePop"       ]]
+  } ### End if(byState)
 
   return(dataList)
 }
