0fab53f9cc7837f1ac4918e0bc778754107d82ba
Tony Gardella <AGardella@indecon.com>
2025-05-13 14:46:41 -0400

    Revert "Extremes module: initialized changes to functions to process and load the extremes data"
    This reverts commit 43fb0ce1e12cbb7e2f9de39b189faa309ecb8571.

--- R/loadFrediData.R
+++ R/loadFrediData.R
@@ -2,52 +2,44 @@
 ### 2022.01.10: Added SLR sectors
 ### This function loads the data from a directory specified by the user. There is a default directory structure it will try to load from.
 loadFrediData <- function(
-    ### Directories
-  dataDir     = "." |> file.path("inst", "extdata"), ### Path to projectDir
-  configDir   = "fredi"    , ### Module directory relative to dataDir
-  scenarioDir = "scenarios", ### Directory to scenario relative to dataDir
-  ### Config info
-  configFile  = "FrEDI_config.xlsx", ### Name of Excel file with config information, relative to frediDir
-  configSheet = "tableNames"       , ### Name of sheet in configFile containing table with info about tables
-  ### Scenario info
-  gcamFile    = "Hector_v5.3_GCAM" |> paste0(".csv"), ### File in scenarioDir containing temperature scenarios
-  gdpFile     = "EPPA_v6_GDP"      |> paste0(".csv"), ### File in scenarioDir containing GDP scenarios
-  popFile     = "ICLUS_State_Population"  |> paste0(".csv"), ### File in scenarioDir containing population scenarios
-  ratiosFile  = "state_population_ratios" |> paste0(".csv"), ### File in scenarioDir containing population ratios
-  testFiles   = list(
-    temp = "temp_0to6_to2300"   |> paste0(".csv"),
-    gdp  = "rff_gdp_mean"       |> paste0(".csv"),
-    pop  = "rff_state_pop_mean" |> paste0(".csv")
-  ), ### Files to load for test scenarios
-  ### Conditionals
-  doScalars   = doScalars  , ### Whether or not do format scalars
-  doScenarios = doScenarios, ### Whether to load scenarios
-  ### Messaging
-  silent      = FALSE, ### Level of messaging
-  msg0        = "\t"   ### Message index
+    mainDir     = "." |> file.path("inst", "extdata"),  ### Path to directory containing configuration data
+    ### Files
+    configFile  = "FrEDI_config.xlsx",                  ### Name of Excel file with config information, relative to frediDir
+    configSheet = "tableNames",                         ### Name of sheet in configFile containing table with info about tables
+    gcamFile    = "Hector_v5.3_GCAM" |> paste0(".csv"), ### File in scenarioDir containing temperature scenarios
+    gdpFile     = "EPPA_v6_GDP"      |> paste0(".csv"), ### File in scenarioDir containing GDP scenarios
+    popFile     = "ICLUS_State_Population"  |> paste0(".csv"), ### File in scenarioDir containing population scenarios
+    ### Other args
+    ratiosFile  = "state_population_ratios" |> paste0(".csv"), ### File in scenarioDir containing population ratios
+    testFiles   = list(
+      temp = "temp_0to6_to2300"   |> paste0(".csv"),
+      gdp  = "rff_gdp_mean"       |> paste0(".csv"),
+      pop  = "rff_state_pop_mean" |> paste0(".csv")
+    ), ### Files to load for test scenarios
+    silent      = FALSE, ### Level of messaging
+    msg0        = "\t"   ### Message index
 ) {
-  ### Set Up Environment -------------------
-  #### Messaging -------------------
+  ###### Messaging ######
   msgN          <- "\n"
   msg1          <- msg0 |> paste("\t")  
   paste0(msg0, "Running loadFrediData...") |> message()
   
-  #### File Paths -------------------
+  ###### File Paths ######
   ### Directories for scalars, GCM scaled impacts, and SLR scaled impacts
-  configDir     <- dataDir   |> file.path(configDir) ### Path to Excel config file, relative to mainDir
-  scenarioDir   <- dataDir   |> file.path(scenarioDir) ### Path to scenarios, relative to mainDir
-  scalarDir     <- configDir |> file.path("scalars"  )
-  impactDirs    <- configDir |> list.files(pattern="gcm|slr")
-  
-  #### Initialize List -------------------
+  frediDir      <- mainDir  |> file.path("fredi"    ) ### Path to Excel config file, relative to mainDir
+  scenarioDir   <- mainDir  |> file.path("scenarios") ### Path to scenarios, relative to mainDir
+  scalarDir     <- frediDir |> file.path("scalars"  )
+  impactDir     <- frediDir
+
+  ###### Initialize List ######
   dataList      <- list()
-  listState     <- list()
   
-  ### Load Table of Tables -------------------
+  ###### Load Table of Tables ######
   ### Load table with names of data tables
   # frediFile |> print()
   if (!silent) paste0(msg1, "Loading configuration data and controls...") |> message()
-  listFrEDI     <- configDir |> loadFrediConfig(
+  listFrEDI     <- loadFrediConfig(
+    configDir   = frediDir   , ### Path to Excel config file relative to mainDir
     configFile  = configFile , ### Name of Excel file with config information, relative to frediDir
     configSheet = configSheet, ### Name of sheet in configFile containing table with info about tables
     silent      = silent     , ### Level of messaging
@@ -56,62 +48,51 @@ loadFrediData <- function(
   ### Add to dataList
   dataList[["frediData"]] <- listFrEDI
   rm(listFrEDI)
+ 
+
+  ###### Load Default Scenarios & Pop Ratios ######
+  if (!silent) paste0(msg1, "Loading scenario data...") |> message()
+  listDefaults  <- loadScenarioData(
+    scenarioDir = scenarioDir, ### Path to Excel config file relative to mainDir
+    gcamFile    = gcamFile   , ### File in scenarioDir containing temperature scenarios
+    gdpFile     = gdpFile    , ### File in scenarioDir containing GDP scenarios
+    popFile     = popFile    , ### File in scenarioDir containing population scenarios
+    ratiosFile  = ratiosFile , ### File in scenarioDir containing population ratios
+    testFiles   = testFiles  , ### Files with scenarios for testing
+    silent      = silent     , ### Level of messaging
+    msg0        = msg1
+  ) ### End loadFrediConfig
+  ### Add to dataList
+  dataList[["scenarioData"]] <- listDefaults
+  rm(listDefaults)
   
-  
-  ### Load Scenarios & Pop Ratios -------------------
-  if(doScenarios) {
-    if (!silent) paste0(msg1, "Loading scenario data...") |> message()
-    listDefaults  <- scenarioDir |> loadScenarioData(
-      gcamFile    = gcamFile   , ### File in scenarioDir containing temperature scenarios
-      gdpFile     = gdpFile    , ### File in scenarioDir containing GDP scenarios
-      popFile     = popFile    , ### File in scenarioDir containing population scenarios
-      ratiosFile  = ratiosFile , ### File in scenarioDir containing population ratios
-      testFiles   = testFiles  , ### Files with scenarios for testing
-      silent      = silent     , ### Level of messaging
-      msg0        = msg1
-    ) ### End loadFrediConfig
-    ### Add to dataList
-    dataList[["scenarioData"]] <- listDefaults
-    rm(listDefaults)
-  } ### End if(doScenarios)
-  
-  
-  ### Load Scalars -------------------
+ 
+  ###### Load Scalars & Scaled Impacts ######
   ### Load scalars
-  if(doScalars) {
-    if (!silent) paste0(msg1, "Loading scalars...") |> message()
-    data_scalars <- scalarDir |> loadFrediScalars()
-    listState[["scalarData"]] <- data_scalars
-    rm(data_scalars)
-  } ### End if(doScalars)
+  if (!silent) paste0(msg1, "Loading scalars...") |> message()
+  data_scalars    <- scalarDir |> loadFrediScalars()
   
+  ### Load GCM scaled impacts
+  if (!silent) paste0(msg1, "Loading GCM scaled impacts...") |> message()
+  data_gcmImpacts <- impactDir |> loadFrediImpacts(type="gcm")
   
-  ###### Load Scaled Impacts -------------------
-  # ### Load GCM scaled impacts
-  # if (!silent) paste0(msg1, "Loading GCM scaled impacts...") |> message()
-  # data_gcmImpacts <- impactDir |> loadFrediImpacts(type="gcm")
-  # listState[["gcmImpData"]] <- data_gcmImpacts
-  # rm(data_gcmImpacts)
-  # ### Load SLR scaled impacts
-  # if (!silent) paste0(msg1, "Loading SLR scaled impacts...") |> message()
-  # data_slrImpacts <- impactDir |> loadFrediImpacts(type="slr")
-  # listState[["slrImpData"]] <- data_slrImpacts
-  # rm(data_slrImpacts)
-  ### Iterate over impact list
-  listImpacts <- impactDirs |> map(function(type0){
-    configDir |> loadFrediImpacts(type=type0)
-  }) |> set_names(impactDirs)
-  ### Add to list
-  listState[["gcmImpData"]] <- listImpacts[["gcm"]]
-  listState[["slrImpData"]] <- listImpacts[["slr"]]
-  rm(listImpacts)
+  ### Load SLR scaled impacts
+  if (!silent) paste0(msg1, "Loading SLR scaled impacts...") |> message()
+  data_slrImpacts <- impactDir |> loadFrediImpacts(type="slr")
   
-  ### Format Return List -------------------
+  ### Create a list
+  listState       <- list()
+  listState[["scalarData"]] <- data_scalars
+  listState[["gcmImpData"]] <- data_gcmImpacts
+  listState[["slrImpData"]] <- data_slrImpacts
+  # rm(data_scalars, data_gcmImpacts, data_slrImpacts)
+  rm(data_scalars, data_slrImpacts)
+  
+  ### Add to dataList
   dataList[["stateData"]] <- listState
   rm(listState)
   
-  ### Return -------------------
-  gc()
+  ###### Return ######
   paste0(msg0, "...Finished running loadFrediData().", msgN) |> message()
   return(dataList)
 }
--- R/reshapeFrediData.R
+++ R/reshapeFrediData.R
@@ -9,9 +9,7 @@
 #'
 #' @examples
 reshapeFrediData <- function(
-    dataList    = NULL , ### List of data (e.g., as returned from FrEDI_Data::loadData())
-    doScalars   = TRUE , ### Whether or not do format scalars
-    doScenarios = FALSE, ### Whether to load scenarios
+    dataList = NULL,   ### List of data (e.g., as returned from FrEDI_Data::loadData())
     silent   = TRUE,   ### Level of messaging
     msg0     = "\t"    ### Prefix for messaging
 ) {
@@ -41,64 +39,45 @@ reshapeFrediData <- function(
   
   ###### Scenarios Data  ######
   ### Reshape scenario data
-  if(doScenarios) {
-    scenarios  <- scenarios |> reshapeScenarioData(
-      silent    = silent, 
-      msg0      = msg1
-    ) ### End reshapeScalarData
-    dataList[["scenarioData"]] <- scenarios
-  } ### End if(doScenarios)
+  scenarios  <- scenarios |> reshapeScenarioData(
+    silent    = silent, 
+    msg0      = msg1
+  ) ### End reshapeScalarData
+  dataList[["scenarioData"]] <- scenarios
   
   ###### Scalar Data  ######
   ### Reshape scalar data
-  if(doScalars) {
-    # stateData |> names() |> print()
-    scalarData <- stateData[["scalarData"]]
-    # scalarData |> glimpse()
-    scalarData <- scalarData |> reshapeScalarData(
-      frediData = frediData, 
-      silent    = silent, 
-      msg0      = msg1
-    ) ### End reshapeScalarData
-    stateData[["scalarData"]] <- scalarData
-  } ### End if(doScalars)
+  # stateData |> names() |> print()
+  scalarData <- stateData[["scalarData"]]
+  # scalarData |> glimpse()
+  scalarData <- scalarData |> reshapeScalarData(
+    frediData = frediData, 
+    silent    = silent, 
+    msg0      = msg1
+  ) ### End reshapeScalarData
+  stateData[["scalarData"]] <- scalarData
   
   ###### GCM Scaled Impacts  ######
   ### Reshape scalar data
-  mList0  <- list()
-  mList0[["mType0"]] <- frediData[["co_modelTypes"]] |> pull(modelType_id) |> unique()
-  mList0[["mName0"]] <- mList0[["mType0"]] |> paste0("ImpData")
-  mData0  <- mList0 |> map(function(mType0, mName0){
-    ### Get data
-    df0 <- stateData[[mName0]]
-    ### Shape data
-    df0 <- df0 |> reshapeScaledImpacts(
-      frediData = frediData, 
-      type0     = mType0,
-      silent    = silent, 
-      msg0      = msg1
-    ) ### End reshapeScalarData
-  }) |> set_names(mList0[["mName0"]])
-  # gcmData <- stateData[["gcmImpData"]]
-  # doGCM   <- !(gcmData |> is.null())
-  # gcmData <- gcmData |> reshapeScaledImpacts(
-  #   frediData = frediData, 
-  #   type0     = "gcm",
-  #   silent    = silent, 
-  #   msg0      = msg1
-  # ) ### End reshapeScalarData
-  # stateData[["gcmImpData"]] <- gcmData
-  # 
-  # ###### SLR Scaled Impacts  ######
-  # ### Reshape scalar data
-  # slrData <- stateData[["slrImpData"]]
-  # slrData <- slrData |> reshapeScaledImpacts(
-  #   frediData = frediData, 
-  #   type0     = "slr",
-  #   silent    = silent, 
-  #   msg0      = msg1
-  # ) ### End reshapeScalarData
-  # stateData[["slrImpData"]] <- slrData
+  gcmData <- stateData[["gcmImpData"]]
+  gcmData <- gcmData |> reshapeScaledImpacts(
+    frediData = frediData, 
+    type0     = "gcm",
+    silent    = silent, 
+    msg0      = msg1
+  ) ### End reshapeScalarData
+  stateData[["gcmImpData"]] <- gcmData
+  
+  ###### SLR Scaled Impacts  ######
+  ### Reshape scalar data
+  slrData <- stateData[["slrImpData"]]
+  slrData <- slrData |> reshapeScaledImpacts(
+    frediData = frediData, 
+    type0     = "slr",
+    silent    = silent, 
+    msg0      = msg1
+  ) ### End reshapeScalarData
+  stateData[["slrImpData"]] <- slrData
   
   ### Update data in list
   dataList[["stateData"]] <- stateData
